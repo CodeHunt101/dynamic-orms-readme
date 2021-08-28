e3bdad39cbe2ae470218a768cd27af37710cb52c
Harold Torres Marino <haroldtm55@gmail.com>
2021-08-28 13:42:07 +1000

    Tutorial completed

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,7 @@ class Song
   def self.column_names
     DB[:conn].results_as_hash = true
 
-    sql = "pragma table_info('#{table_name}')"
+    sql = "PRAGMA table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
     column_names = []
@@ -56,7 +56,7 @@ class Song
   def self.find_by_name(name)
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
-  end
+  end 
 
 end
 
